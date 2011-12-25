56feb1dec86db0aba2293fdf91cad3c13340f8b9
Brian Dawn <brian.t.dawn@gmail.com>
2011-12-24 19:52:18 -0600

    Added effect plugins, reogranized the plugin directories.

---
+++ plugins/effects/fire.py
@@ -0,0 +1,45 @@
+import sf
+
+import effects
+
+
+# An example of an effect plugin.
+class Effect(effects.Effect):
+    
+    ID = 19242829
+    
+    def __init__(self, x, y):
+        super(Effect, self).__init__(x, y)
+        
+    
+    def _update_particle(self, p):
+        color = p.sprite.color
+        
+        if color.a >= 4:
+            color.a = p.sprite.color.a - 4
+            
+        if color.r >= 4:
+            color.r = p.sprite.color.r - 4
+        
+        p.sprite.color = color
+
+        
+    def _update(self):
+
+        p = effects.Particle(media.get_texture("particle"))
+        
+        p.life = 80
+        #p.sprite.blend_mode = sf.BlendMode.ADD
+        p.sprite.color = sf.Color(255,5,155)
+        p.sprite.scale(2, 2)
+
+        p.x_velocity = random.randrange(-100, 100) / 100.0
+        p.y_velocity = random.randrange(-100, 100) / 100.0
+        
+        
+        self._particles.append(p)
+
+
+import random
+import media
+    
